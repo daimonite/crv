b54f4a03ebb6a2466712e885693fcfa34e74af90
Kelvin Daimon <kelvindaimon2908@gmail.com>
2026-08-27 13:25:53 +0300

    fix: allow 172.16.0.2 dev origin

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["127.0.0.1", "localhost", "192.168.1.117"],
+  allowedDevOrigins: ["127.0.0.1", "localhost", "192.168.1.117", "172.16.0.2"],
 
   images: {
     remotePatterns: [
